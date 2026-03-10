ef368faab50bb2d6556249fa11ab4bf58f3596f4
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2026-03-10 14:39:54 +0100

    Update InfoCard.tsx

--- lumina/src/components/infoCard/InfoCard.tsx
+++ lumina/src/components/infoCard/InfoCard.tsx
@@ -2,27 +2,29 @@ import WeatherBoard from "../weatherBoard/WeatherBoard";
 
 function InfoCard({weatherData }: any) {
     return (
-        <nav className="
+        <div className="
         fixed 
-        top-30 
-        left-10 
-        right-10 
+        top-[10vh]
+        left-2 md:left-10
+        right-2 md:right-10
         z-[100] 
         flex 
         justify-between 
-        px-10  
+        px-4 md:px-10
         py-8
         backdrop-blur-md 
         border-b
         border-white/10
         text-white
         rounded-3xl
+        mx-auto //centratura
+        max-w-6xl           // Evita troppo larga su schermi max
         "
             style={{ height: '80vh' }}
         >
             <WeatherBoard weatherData={weatherData}/>
 
-        </nav>
+        </div>
     );
 }
 
